c4e8e839ce969e2177d6fee3510b68699c99ff4e
orkcel <orkhancelestial@gmail.com>
2024-03-26 21:39:08 +0400

    LAST COMMIT / FIXED BUGS WITH MEDIA STYLES / v1.0

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const MOBILE_BUTTON = document.getElementById("mobileButton");
   const MOBILE_MENU = document.getElementById("mobileMenu");
+  const MOBILE_CLOSE = document.getElementById("mobileClose");
+
   MOBILE_BUTTON.addEventListener("click", () => {
     MOBILE_MENU.classList.add("mobile-menu-active");
-    const MOBILE_CLOSE = document.getElementById("mobileClose");
     MOBILE_CLOSE.addEventListener("click", () => {
       MOBILE_MENU.classList.remove("mobile-menu-active");
     });
